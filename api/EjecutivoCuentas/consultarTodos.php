744d7b25a7d7017bd97f85ef7b8397f6d12f3f22
N74Mmerino <nnicolasmmerino@gmail.com>
2024-11-26 15:15:33 -0300

    1er commit consultar todos

--- api/EjecutivoCuentas/consultarTodos.php
+++ api/EjecutivoCuentas/consultarTodos.php
@@ -3,8 +3,26 @@
 header('Content-Type: application/json');
 
 require_once 'modelosRespuestas/consultarTodosRespuesta.php';
+require_once '../../modelo/ejecutivoCuenta.php';
 
 $resp= new ConsultarTodosRespuesta();
+
+$le1= new EjecutivoCuenta();
+$le1->Id=1;
+$le1->Descripcion="Ejecutivo 1";
+$le1->Usuario="comercial";
+
+$le2= new EjecutivoCuenta();
+$le2->Id=1;
+$le2->Descripcion="Ejecutivo Zona Sur";
+$le2->Usuario="Javkin";
+
+$resp->ListEjecutivosDeCuentas[]=$le1;
+$resp->ListEjecutivosDeCuentas[]=$le2;
+
+
+
+
 
 
 
--- api/EjecutivoCuentas/modelosRespuestas/consultarTodosRespuesta.php
+++ api/EjecutivoCuentas/modelosRespuestas/consultarTodosRespuesta.php
@@ -1,5 +1,7 @@
 <?php
 
 class ConsultarTodosRespuesta{
+
+    public $ListEjecutivosDeCuentas =array();
     
 }
