fff5466f9fa34947a2e1b326af08fa0e9256af58
N74Mmerino <nnicolasmmerino@gmail.com>
2024-11-26 15:23:01 -0300

    1er commit consultar

--- api/EjecutivoCuentas/consultar.php
+++ api/EjecutivoCuentas/consultar.php
@@ -3,11 +3,16 @@
 header('Content-Type: application/json');
 
 require_once 'modelosRespuestas/consultarRespuesta.php';
+require_once '../../modelo/ejecutivoCuenta.php';    
 
 
 $resp= new ConsultarRespuesta();
 
+$ec= new EjecutivoCuenta();
+$ec->Id= $_GET['Id'];
+$ec->Descripcion="Ejecutivo 1";
+$ec->Usuario="comercial";
 
-
+$resp->EjecutivoDeCuenta= $ec;
 
 echo json_encode ($resp);
